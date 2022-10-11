8e44d79479993e38df8e15fb3c59440862459c11
Douglas Ribeiro <dgrb0325@gmail.com>
2022-10-11 14:40:03 -0300

    creates users routes

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,17 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const { user_id } = request.headers;
+      // console.log(id);
+      const id = user_id as string;
+
+      const users = this.listAllUsersUseCase.execute({ user_id: id });
+
+      return response.json(users);
+    } catch (err) {
+      return response.status(400).json({ error: err });
+    }
   }
 }
 
